5879639f39ea32975be101e12132a2ef9db7bd21
souemya <40347869+souemya@users.noreply.github.com>
2019-12-07 11:40:15 +0100

    Fichier pour les requêtes
    Se référer au tableau des fonctionnalité pour le compléter

---
+++ requetesSQL.sql
@@ -0,0 +1,20 @@
+
+--- S'inscrisre à une conf [Auditeur]
+INSERT INTO Inscription VALUES 
+						('SELECT idUser FROM Utilisateurs WHERE idUser =: idUser',
+						'SELECT idConference FROM Conference WHERE idConference = :idConference')
+						);
+
+
+
+--- Se désinscrire d'une conf [Auditeur]
+DELETE Inscription VALUES 
+						('SELECT idUser FROM Utilisateurs WHERE idUser =: idUser',
+						'SELECT idConference FROM Conference WHERE idConference = :idConference')
+						);
+
+
+
+--- Filtrer conf [Auditeur]
+SELECT nomConf,theme FROM conference WHERE theme =: themechoisi OR nomConf =:motsCles OR
+						SELECT Nom,Prenom,Pseudo FROM Utilisateurs WHERE Speaker == true AND Nom =: motEntre OR Prenom=: motEntre OR Pseudo =: motEntre);
