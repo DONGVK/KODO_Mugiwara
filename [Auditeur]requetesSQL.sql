7278e7f9a017b388eb65b0a5f900cbdc3411d227
Kenza Zerouali <49944467+kenza24@users.noreply.github.com>
2019-12-11 11:14:37 +0100

    Update [Auditeur]requetesSQL.sql

--- [Auditeur]requetesSQL.sql
+++ [Auditeur]requetesSQL.sql
@@ -6,7 +6,22 @@ INSERT INTO Inscription VALUES
 						('SELECT idUser FROM Utilisateurs WHERE idUser =: idUser',
 						'SELECT idConference FROM Conference WHERE idConference = :idConference')
 						);
+-- ou (php) 
 
+public function sinscrire (){
+      //utilisation de la fonction password_hash pour pouvoir stocker les mdp mais pas en clair
+      $mdp_hash = password_hash ($POST ["motDePasse"], PASSWORD_DEFAULT);
+
+      $req = $bdd -> prepare ("insert into user (idUser, nom, prenom, motDePasse, adresseMail) VALUES (?, ?, ?, ?, ?)");
+      $req -> execute (array (
+        'mdp'=> $mdp_hash,
+        'id'=> $idUser,
+        'nom'=> $nom,
+        'prenom'=> $prenom
+        'adresseMail'=> $adresse,
+      ) );
+      }
+    }
 
 
 --- Se désinscrire d'une conf [Auditeur]
@@ -20,3 +35,6 @@ DELETE Inscription VALUES
 --- Filtrer conf [Auditeur]
 SELECT nomConf,theme FROM conference WHERE theme =: themechoisi OR nomConf =:motsCles OR
 						SELECT Nom,Prenom,Pseudo FROM Utilisateurs WHERE Speaker == true AND Nom =: motEntre OR Prenom=: motEntre OR Pseudo =: motEntre);
+						
+						
+--- Affichage des personnes suivi
