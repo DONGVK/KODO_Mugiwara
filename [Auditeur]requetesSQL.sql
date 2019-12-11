9114fda8d62e5eb09c4b164bc37e9be8aade69d6
Kenza Zerouali <49944467+kenza24@users.noreply.github.com>
2019-12-11 11:41:43 +0100

    Update [Auditeur]requetesSQL.sql

--- [Auditeur]requetesSQL.sql
+++ [Auditeur]requetesSQL.sql
@@ -12,7 +12,7 @@ public function sinscrire (){
       //utilisation de la fonction password_hash pour pouvoir stocker les mdp mais pas en clair
       $mdp_hash = password_hash ($POST ["motDePasse"], PASSWORD_DEFAULT);
 
-      $req = $bdd -> prepare ("insert into user (idUser, nom, prenom, motDePasse, adresseMail) VALUES (?, ?, ?, ?, ?)");
+      $req = $bdd -> prepare ("insert into Inscription (idUser, nom, prenom, motDePasse, adresseMail) VALUES (?, ?, ?, ?, ?)");
       $req -> execute (array (
         'mdp'=> $mdp_hash,
         'id'=> $idUser,
@@ -38,6 +38,10 @@ SELECT nomConf,theme FROM conference WHERE theme =: themechoisi OR nomConf =:mot
 						
 						
 --- Affichage des personnes suivi [Auditeur]
-SELECT 
+-- je sais pas comment faire pour les recuperer ? faut pas qu'on rajoute un attribut "personneSuivi" dans Utilisateur, si oui alors :
+SELECT personneSuivi FROM Utilisateur WHERE id:= idUser; 
+
+--- Affichage des évènements auxquels il a participé [Auditeur]
+SELECT idConference, nomConf, dateConf FROM Conference WHERE id =: idUser; 
 
 
