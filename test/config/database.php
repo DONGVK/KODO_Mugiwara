9a8d7bccef37d9c727f0ae26736d6a7551a04442
AudreyDM <49957132+AudreyDM@users.noreply.github.com>
2019-12-19 12:54:41 +0100

    Update database.php

--- test/config/database.php
+++ test/config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'postgresgl'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
