1a015e86b75340fcebbfce0f293d694da433ef46
souemya <40347869+souemya@users.noreply.github.com>
2019-12-25 21:59:41 +0100

    pages test

---
+++ Kodo_back/resources/views/publication.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.corphtml', ['title' => 'Publication'])
+
+@section('bloc_body')
+<h1>publication</h1>
+{{'test'}}
+
+<p><a href="http://localhost:8000/"> retour Accueil </a></p>
+@endsection
