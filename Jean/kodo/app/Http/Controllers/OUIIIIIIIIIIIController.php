81655dc0b574cd6a452d3a4426f442d4e9ceacd3
DONGVK <57867631+DONGVK@users.noreply.github.com>
2020-01-07 16:34:27 +0100

    update
    Model

---
+++ Jean/kodo/app/Http/Controllers/OUIIIIIIIIIIIController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OUIIIIIIIIIII;
+use Illuminate\Http\Request;
+
+class OUIIIIIIIIIIIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\OUIIIIIIIIIII  $oUIIIIIIIIIII
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OUIIIIIIIIIII $oUIIIIIIIIIII)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\OUIIIIIIIIIII  $oUIIIIIIIIIII
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OUIIIIIIIIIII $oUIIIIIIIIIII)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\OUIIIIIIIIIII  $oUIIIIIIIIIII
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OUIIIIIIIIIII $oUIIIIIIIIIII)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\OUIIIIIIIIIII  $oUIIIIIIIIIII
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OUIIIIIIIIIII $oUIIIIIIIIIII)
+    {
+        //
+    }
+}
